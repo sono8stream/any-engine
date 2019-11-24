26acc665facdcf552e417cfd49ebb4cf692aeea3
sono8stream <sono.stream8580@gmail.com>
2019-11-24 21:37:01 +0900

    Remove unused eval code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import React, { useEffect } from 'react';
-import { Canvas } from 'react-three-fiber';
-import * as ts from 'typescript';
-import ScriptTag from './component/scriptTag';
+import React from 'react';
 import Field from './demo/Field';
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const source = "const x : string = 'string';console.log(x);";
-    const result = ts.transpileModule(source, {
-      compilerOptions: { module: ts.ModuleKind.CommonJS }
-    });
-    eval(result.outputText);
-  }, []);
   return (
     <div className="App">
-      <ScriptTag src="./testScript.js" />
       <Field />
     </div>
   );
